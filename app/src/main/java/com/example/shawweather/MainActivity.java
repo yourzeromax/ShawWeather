e0bb943401a3e64bc1529c6bc2fb6f0c586666ba
yourzeromax <yourzeromax@163.com>
2017-09-24 10:49:43 +0800

    1
    1

--- app/src/main/java/com/example/shawweather/MainActivity.java
+++ app/src/main/java/com/example/shawweather/MainActivity.java
@@ -1,34 +1,31 @@
 package com.example.shawweather;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
-
-import com.example.shawweather.util.Utility;
 
 
 public class MainActivity extends baseActivity {
     public static String old_id;
-    public static int SINGLE=1;
+    public static int SINGLE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if(prefs.getString("weather",null)!=null){
-            if(SINGLE==1){
-            Intent intent= new Intent(this,WeatherActivity.class);
-            startActivity(intent);
-            finish();}else if (SINGLE==0){
+        if (prefs.getString("weather", null) != null) {
+            if (SINGLE == 1) {
+                Intent intent = new Intent(this, WeatherActivity.class);
+                startActivity(intent);
+                finish();
+            } else if (SINGLE == 0) {
                 SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("weather",null).apply();
-                SINGLE=1;
+                editor.putString("weather", null).apply();
+                SINGLE = 1;
             }
         }
     }
@@ -40,9 +37,10 @@ public class MainActivity extends baseActivity {
     @Override
     public void onBackPressed() {
 
-        Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
-        intent.putExtra("weather_id",old_id);
-        startActivity(intent);
-        finish();
+      //  Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+       // intent.putExtra("weather_id", old_id);
+       // startActivity(intent);
+       // finish();
+        Toast.makeText(this, "请选择城市！", Toast.LENGTH_SHORT).show();
     }
 }
